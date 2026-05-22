cfa922451fb004ba8a6bd737a068c10772c8a69b
Valentina Luna <lourdes.lunaacosta@alu.frt.utn.edu.ar>
2026-05-22 19:58:48 -0300

    Ejemplo Dictionary realizado

--- Dsw2026Ej11/Collections/CasoDictionary.cs
+++ Dsw2026Ej11/Collections/CasoDictionary.cs
@@ -7,4 +7,36 @@
 //Incluir un método para eliminar un alumno utilizando la clave
 public class CasoDictionary
 {
+    private static Dictionary<int, string> alumnos = new Dictionary<int, string>();
+    public static void AgregarAlumno(int legajo, string nombre)
+    {
+        if (!alumnos.ContainsKey(legajo))
+        {
+            alumnos.Add(legajo, nombre);
+        }
+        else
+        {
+            Console.WriteLine("El legajo ya existe. No se puede agregar el alumno.");   
+        }
+
+    }
+    public static string BuscarAlumno(int legajo)
+    {
+        if (alumnos.TryGetValue(legajo, out string nombre))
+        {
+            return nombre;
+        }
+        else
+        {
+            return "No existe";
+        }
+    }
+    public static Dictionary<int, string> RetornarDiccionario()
+    {
+        return alumnos;
+    }
+    public static bool EliminarAlumno(int legajo)
+    {
+        return alumnos.Remove(legajo);
+    }
 }
--- Dsw2026Ej11/Tests/Ejemplos.cs
+++ Dsw2026Ej11/Tests/Ejemplos.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Tests;
+﻿using Dsw2026Ej11.Collections;
+
+namespace Dsw2026Ej11.Tests;
 
 
 internal class Ejemplos
@@ -21,6 +23,44 @@ internal class Ejemplos
     //Eliminar un alumno por clave y listar por consola los alumnos
     public static void EjemploDictionary()
     {
+        CasoDictionary.AgregarAlumno(57892,"Valentina Luna");
+        CasoDictionary.AgregarAlumno(57893,"Javier Paz");
+        CasoDictionary.AgregarAlumno(57894,"Ana García");
+        Console.WriteLine("Alumnos:");
+        foreach (var alumno in CasoDictionary.RetornarDiccionario())
+        {
+            Console.WriteLine($"Legajo: {alumno.Key}, Nombre: {alumno.Value}");
+        }       
+        Console.WriteLine("\nBuscar alumno por legajo 57893:");
+        foreach (var alumno in CasoDictionary.RetornarDiccionario())
+        {
+            if (alumno.Key == 57893)
+            {
+                Console.WriteLine($"Legajo: {alumno.Key}, Nombre: {alumno.Value}");
+            }
+        }
+        Console.WriteLine("\nBuscar alumno por legajo 57895:");
+        foreach (var alumno in CasoDictionary.RetornarDiccionario())
+        {
+            if (alumno.Key == 57895)
+            {
+                Console.WriteLine($"Legajo: {alumno.Key}, Nombre: {alumno.Value}");
+            }
+            else
+            {
+                Console.WriteLine("No existe");
+                break;
+            }
+        }
+        Console.WriteLine("\nEliminar alumno por legajo 57892:");
+        CasoDictionary.EliminarAlumno(57892);
+        Console.WriteLine("Alumnos:");
+        foreach (var alumno in CasoDictionary.RetornarDiccionario())
+        {
+            Console.WriteLine($"Legajo: {alumno.Key}, Nombre: {alumno.Value}");
+        }
+
+
 
     }
 
